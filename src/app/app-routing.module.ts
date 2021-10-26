ceee6c3137b10cc5b1373ac08430083d6b5e8ef3
Miruna Popescu <miru.popescu@yahoo.fr>
2021-10-26 09:36:04 +0200

    modif index, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: "accueil", component: AccueilComponent},
+  // { path: "gymnase", component: GymnaseListPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
